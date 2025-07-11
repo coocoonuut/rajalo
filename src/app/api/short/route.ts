3fbf2fc4e7216012916dfa50a790e8be92dcccc6
coocoonuut <goddmartee@gmail.com>
2025-07-10 19:09:49 -0600

    chore(API Short): refactor try/catch and NextResponse reused function

--- src/app/api/short/route.ts
+++ src/app/api/short/route.ts
@@ -1,32 +1,27 @@
 import jsonResponse from "@/lib/jsonResponse";
 import { prisma } from "@/lib/prisma";
 import { nanoid } from "nanoid";
-import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
+  const { longUrl } = await request.json();
+
+  // Validate input
+  if (!longUrl || typeof longUrl !== "string") {
+    return jsonResponse({ error: "longUrl must be a valid string" }, 400);
+  }
+
   try {
-    const { longUrl } = await request.json();
-
-    // Validate input
-    if (!longUrl || typeof longUrl !== "string") {
-      return jsonResponse({ error: "longUrl must be a valid string" }, 400);
-    }
-
-    // Create a new temporal URL entry
     const newTemporalUrl = await prisma.temporalUrls.create({
       data: {
         longUrl,
         codeUrl: nanoid(6),
-        expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        expiresAt: new Date(Date.now() + 30 * 1000),
       },
     });
 
-    return NextResponse.json(newTemporalUrl, { status: 201 });
+    return jsonResponse(newTemporalUrl, 201);
   } catch (error) {
     console.error("Error creating temporal URL:", error);
-    return NextResponse.json(
-      { error: "Failed to create temporal URL" },
-      { status: 500 }
-    );
+    return jsonResponse({ error: "Failed to create temporal URL" }, 500);
   }
 }
