28eab47c45df1ae9ba208746bcfc978f0f3a136c
coocoonuut <goddmartee@gmail.com>
2025-07-17 14:17:25 -0600

    fix(app): fix hot reloading for differents OS containers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+  },
 };
 
 export default nextConfig;
