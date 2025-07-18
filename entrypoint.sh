ce3d4582568c447de70d2f4a88eb58476f69f976
coocoonuut <goddmartee@gmail.com>
2025-07-17 18:20:19 -0600

    fix(docker): ensure database readiness before prisma migrate deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-echo "Running Prisma migrations..."
+set -e # End the process on any failure
 
+DB_HOST="database"
+DB_PORT="3306"
+
+echo "\nWaiting for database to be ready at ${DB_HOST}:${DB_PORT}..."
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
+  sleep 1
+done
+echo "\nDatabase is ready!"
+
+echo "\nRunning Prisma migrations..."
 npx prisma migrate deploy
 
-echo "Starting Next.js app..."
+echo "\nStarting Next.js app...\n"
 
 exec "$@" # Replace this process with the following instructions...
